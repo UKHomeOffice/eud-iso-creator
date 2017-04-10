ceee92030c43693c7aef3d43088f022db663cdf4
Yousof El-Sayed <yousof.el-sayed@homeoffice.gsi.gov.uk>
2017-04-10 15:10:13 +0100

    Ported a lot of the standard eud ansible roles into scripts

---
+++ config/chroot.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+# This is a script to configure a fresh ubuntu image as per the standards here:
+# https://github.com/UKHomeOffice/development_environment
+
+# Find script directory:
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
+# Import functions
+. image_config/base
+. image_config/generic
+. image_config/gui
+. image_config/os_hardening
+. image_config/ssh_hardening
+. image_config/ip_tables
+. image_config/grub_cmdline
+. image_config/audit_hardening
+. image_config/open_jdk
+. image_config/docker
+. image_config/kubectl_minikube
+. image_config/virtualbox_role
+. image_config/vagrant
+. image_config/packer
+
+echo "Configuring image to make a bootable installation disk"
+  separator
+  update_and_install_base_packages
+  set_kernel_version
+
+echo "Performing generic role to install packages / configure system"
+  separator
+  generic_role
+
+echo "Installing packages related to GUI"
+  separator
+  gui_role
+
+echo "Performing OS hardening tasks"
+  separator
+  os_hardening_role
+
+echo "Performing SSH hardening tasks"
+  separator
+  ssh_hardening_role
+
+echo "Configuring ip tables"
+  separator
+  ip_tables_role
+
+echo "Configuring grub cmdline"
+  separator
+  grub_cmdline_role
+
+echo "Performing Audit hardening tasks"
+  separator
+  audit_hardening_role
+
+echo "Installing OpenJDK"
+  separator
+  open_jdk_role
+
+echo "Installing Docker"
+  separator
+  docker_role
+
+echo "Installing Kubectl & Minikube"
+  separator
+  kubectl_minikube_role
+
+echo "Installing Virtual Box"
+  separator
+  virtualbox_role
+
+echo "Installing Vagrant"
+  separator
+  vagrant_role
+
+echo "Installing Packer"
+  separator
+  packer_role
+
+exit 0
---
+++ create_iso.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+# This is a script to generate a usable ISO that is functionally the same as:
+# https://github.com/UKHomeOffice/development_environment
+
+check_for_requirements() {
+  PACKAGES=$(dpkg -l)
+  if [[ -z $(echo "$PACKAGES"| grep "debootstrap") || \
+	  -z $(echo "$PACKAGES" | grep "xorriso") || \
+	  -z $(echo "$PACKAGES" | grep "squashfs-tools") ]]; then
+    separator
+    echo "This script requires you have debootstrap, xorriso and squashfs-tools installed,"
+    echo "please install them then come back and try again."
+    exit 1
+  fi
+  if [[ "$EUID" -ne 0 ]]; then
+    echo "This script requires you run it as root"
+    echo "Please check your permissions then come back and try again"
+    exit 1
+  fi
+  echo "Looks good, carrying on"
+}
+
+prepare_directories() {
+  export WORK=~/work
+  export CD=~/cd
+  export FORMAT=squashfs
+  export FS_DIR=casper
+  mkdir -p ${CD}/{${FS_DIR},boot/grub} ${WORK}/rootfs
+}
+
+pull_down_image() {
+  debootstrap xenial ${WORK}/rootfs
+}
+
+mount_filesystem() {
+  mount --bind /dev ${WORK}/rootfs/dev
+  mount -t proc proc ${WORK}/rootfs/proc
+  mount -t sysfs sysfs ${WORK}/rootfs/sys
+}
+
+unmount_filesystem() {
+  umount ${WORK}/rootfs/proc
+  umount ${WORK}/rootfs/sys
+  umount ${WORK}/rootfs/dev
+}
+
+prepare_image_for_use() {
+  mkdir -p ${WORK}/rootfs/install/
+  rsync -a config/sources/*     ${WORK}/rootfs/etc/apt/sources.list.d/
+  mkdir -p                      ${WORK}/rootfs/root/install/image_config/
+  rsync -a config/chroot.sh     ${WORK}/rootfs/root/install/
+  rsync -a config/image_config/ ${WORK}/rootfs/root/install/image_config/
+}
+
+configure_image() {
+  chroot ${WORK}/rootfs /root/install/chroot.sh
+  export kversion=`cd ${WORK}/rootfs/boot && ls -1 vmlinuz-* | tail -1 | sed 's@vmlinuz-@@'`
+  cp -vp ${WORK}/rootfs/boot/vmlinuz-${kversion} ${CD}/${FS_DIR}/vmlinuz
+  cp -vp ${WORK}/rootfs/boot/initrd.img-${kversion} ${CD}/${FS_DIR}/initrd.img
+  cp -vp ${WORK}/rootfs/boot/memtest86+.bin ${CD}/boot
+}
+
+prepare_cd_directory() {
+  chroot ${WORK}/rootfs dpkg-query -W --showformat='${Package} ${Version}\n' | \
+	                                 sudo tee ${CD}/${FS_DIR}/filesystem.manifest
+  cp -v ${CD}/${FS_DIR}/filesystem.manifest{,-desktop}
+  REMOVE='ubiquity casper user-setup os-prober libdebian-installer4'
+  for i in $REMOVE ; do
+    sudo sed -i "/${i}/d" ${CD}/${FS_DIR}/filesystem.manifest-desktop
+  done
+  mksquashfs ${WORK}/rootfs ${CD}/${FS_DIR}/filesystem.${FORMAT} -noappend
+  echo -n $(sudo du -s --block-size=1 ${WORK}/rootfs | tail -1 | awk '{print $1}') \
+	                                  | sudo tee ${CD}/${FS_DIR}/filesystem.size
+  find ${CD} -type f -print0 | xargs -0 md5sum | sed "s@${CD}@.@" | \
+	                           grep -v md5sum.txt | sudo tee -a ${CD}/md5sum.txt
+  rsync -a config/image_config/files/grub.cfg ${CD}/boot/grub/grub.cfg
+  grub-mkrescue -o ~/eud-environment-$(date +%Y-%m-%d-%H-%M-%S).iso ${CD}
+}
+
+separator() {
+  echo "################################################################################"
+}
+
+echo "Checking for requirements"
+  separator
+  check_for_requirements
+
+echo "Preparing work directories"
+  separator
+    prepare_directories
+  separator
+
+echo "Downloading Ubuntu"
+  separator
+    pull_down_image
+    prepare_image_for_use
+  separator
+
+echo "Configuring image"
+  separator
+    mount_filesystem
+    configure_image
+    unmount_filesystem
+  separator
+
+echo "Preparing the filesystem for the creation of the ISO"
+  separator
+    prepare_cd_directory
+  separator
+
+exit 0
